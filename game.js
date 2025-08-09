f25005c6d5d80be81fe49886b4ffca76a276110c
Tatiana Araque <LTARAQUEU@ACADEMIA.USBBOG.EDU.CO>
2025-08-09 12:55:07 -0500

    Upgrade: Added a new funcionality to the game

--- game.js
+++ game.js
@@ -9,17 +9,21 @@ function jugar() {
     console.log("Adivina el número correcto entre 1 y 100.");
 
     const numeroCorrecto = Math.floor(Math.random() * 100) + 1;
+    let intentos = 0; // contador de intentos
 
     function preguntarNumero() {
         rl.question("Numero: ", (input) => {
             const intento = parseInt(input);
+            intentos++; // aumentar cada vez que intenta
+
             if (isNaN(intento) || intento < 1 || intento > 100) {
                 console.log(" Es un numero entre el 1 y el 100, pdj");
                 preguntarNumero();
                 return;
             }
+
             if (intento === numeroCorrecto) {
-                console.log("EPAAAAA");
+                console.log(`EPAAAAA, lo lograste en ${intentos} intentos.`);
                 jugarDeNuevo();
             } else {
                 const diferencia = Math.abs(numeroCorrecto - intento);
